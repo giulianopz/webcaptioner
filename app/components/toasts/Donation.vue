9289769ec26b3dfadaf682c52ca512d72bf5672b
giuliano <panzironi.giuliano@gmail.com>
2023-11-14 00:44:49 +0100

    Remove donation message from toast popup

--- app/components/toasts/Donation.vue
+++ app/components/toasts/Donation.vue
@@ -1,58 +1,10 @@
 <template>
-  <toast
-    title="Support Web Captioner"
-    :show="$store.state.donation.message.show"
-  >
+  <toast title="Support Web Captioner" :show="$store.state.donation.message.show">
     <p>I hope you've been enjoying Web Captioner!</p>
     <p>
-      Generous donations keep Web Captioner going. If you've found this helpful,
-      please consider donating.
+      If you've found it helpful, please consider contributing to the <a
+        href="https://github.com/giulianopz/webcaptioner">project</a>.
     </p>
-    <div class="mb-n2">
-      <b-button
-        size="sm"
-        variant="secondary"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >$2.50</b-button
-      ><b-button
-        size="sm"
-        variant="secondary"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >$5</b-button
-      ><b-button
-        size="sm"
-        variant="secondary"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >$10</b-button
-      ><b-button
-        size="sm"
-        variant="secondary"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >$20</b-button
-      ><b-button
-        size="sm"
-        variant="secondary"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >Other amount</b-button
-      ><b-button
-        size="sm"
-        variant="outline-info"
-        class="mr-2 mb-2"
-        to="/donate"
-        target="_blank"
-        >More info</b-button
-      >
-    </div>
   </toast>
 </template>
 
